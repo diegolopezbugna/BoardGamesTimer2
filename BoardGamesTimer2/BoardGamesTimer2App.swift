6cdb826460bf58d4a8b549791c18805742f6872e
Diego López Bugna <diegolopezbugna@gmail.com>
2026-04-20 11:41:11 -0300

    Initial Commit

---
+++ BoardGamesTimer2/BoardGamesTimer2App.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGamesTimer2App.swift
+//  BoardGamesTimer2
+//
+//  Created by Diego López Bugna on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct BoardGamesTimer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoardGamesTimer2Tests/BoardGamesTimer2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGamesTimer2Tests.swift
+//  BoardGamesTimer2Tests
+//
+//  Created by Diego López Bugna on 20/04/2026.
+//
+
+import Testing
+@testable import BoardGamesTimer2
+
+struct BoardGamesTimer2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
